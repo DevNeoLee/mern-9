5f5655ce487aab5b59fd9a5cb318174c062bd2e9
DevNeoLee <devneolee@gmail.com>
2022-04-25 22:27:40 -0700

    Update Popup Erica

--- frontend/src/components/EricaPopup.js
+++ frontend/src/components/EricaPopup.js
@@ -31,7 +31,7 @@ export default function EricaPopup({setPopup}) {
                     </div>
                     <p>
                         Please hover your mouse over items on the map to analyze the status of every players on the city</p>
-                    <p> 1) Decide How much is the Risk for Evacuation to the city</p>
+                    <p> 1) Decide How much the Risk for Evacuation to the city</p>
                     <p>2) Submit your decisions in typed words to Pete and Normans</p>
                     <div className="buttons" style={{ margin: "15px 80px" }}><Button size="lg" onClick={() => setPopup(false)}>Start</Button></div>
                 </animated.div>
